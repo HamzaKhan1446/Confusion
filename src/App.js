d9eb780aafebeacf1ef26703c07b9b71d6ab6809
Hamza Khan <k164072@nu.edu.pk>
2020-10-27 21:41:30 +0500

    2.Component Part1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Navbar , NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent'
 
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component{
+
+
+  render()
+  {
+    return(
+      <div className="App">
 
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="https://www.google.com.pk/"> Confusion App</NavbarBrand>
         </div>
       </Navbar>
+
+      <Menu></Menu>
      
     </div>
-  );
+    );
+  }
 }
 
 export default App;
