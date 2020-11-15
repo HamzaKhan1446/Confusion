aac3eba599ec4ebfec9bc520addf664e9b3af208
Hamza Khan <k164072@nu.edu.pk>
2020-11-16 01:26:49 +0500

    5.React Routing

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {Navbar , NavbarBrand} from 'reactstrap';
-import Dishdetail from '../components/DishdetailComponent/DishdetailComponent';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from'./HeaderComponent';
 import Footer from './FooterComponent';
 
@@ -14,21 +14,29 @@ class Main extends Component {
     super(props)
     this.state = {
         dishes: DISHES,
-        selectedDish: null
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+ 
 
   render() {
+
+
+    const HomePage = () => {
+      return(
+          <Home 
+          />
+      );
+    }
     return (
       <div>
         <Header></Header>
-      
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+        </Switch>
 
         <Footer></Footer>
       </div>
